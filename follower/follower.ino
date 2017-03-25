5833b7f0383a5e2b826924090640bfb7e64ea010
Fablab Imperia <fablabimperia@gmail.com>
2017-03-26 00:51:15 +0100

    ignorata cartella di build per build con vscode

--- follower/follower.ino
+++ follower/follower.ino
@@ -1,6 +1,17 @@
 #include "LineDetector.h"
 #include "AFMotor.h"
 
+
+//VARIABILI CONTROLLO MOTORI
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
+
+//Rilevatore di linee
+LineDetector sensorbar;
+
+
 void setup() {
   // put your setup code here, to run once:
 
